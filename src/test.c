0d0b86534bded051c8e9c1777581804f137044d3
tditada <tditada@gmail.com>
2014-06-18 13:15:47 -0300

    structures

---
+++ src/test.c
@@ -0,0 +1,39 @@
+#define MAX_INODES 512
+#define MAX_IMAP 512
+
+struct data {
+	char * text;
+	int inum;
+	int offset;
+	void * next; //next block on the log
+};
+
+typedef data * pdata;
+
+struct inode{
+	int num;
+	pdata[MAX_DATA] idata;
+	void * next;
+};
+
+typedef inode * pinode;
+
+struct imap{
+	pinode[MAX_INODES] map;
+	void * next;
+};
+
+typedef imap * pimap;
+
+struct checkpoint{
+	pimap[MAX_IMAP] map;
+	void * headsegment;
+	void * tailsegment;
+};
+
+
+int init(){
+	// Hacemos el CR en RAM
+	// 
+
+}
