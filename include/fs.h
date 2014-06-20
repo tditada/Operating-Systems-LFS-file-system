30d67f9dd713dc90ff78093238fea1d138dd9d4c
kpjjpk <juanpablorey.10@gmail.com>
2014-06-20 18:25:11 -0300

    Solving merge problems

--- include/fs.h
+++ include/fs.h
@@ -2,29 +2,24 @@
 #define FS_H
 #include "kernel.h"
 
+#define DATA_BLOCK_SIZE 128
 #define MAX_INODES 512
 #define MAX_IMAP 512 // ??? shouldn't they be the same?
 #define BUFFER_SIZE 5*SECTOR_SIZE // get an actual number for this, there's a formula!
 #define MAX_PATH 256
 
 typedef struct {
-	char * text;
-	int inum;
-	int offset;
-	void * next; //next block on the log
-} file_data;
+	char[DATA_BLOCK_SIZE] data;
+} fdata;
 
 typedef struct {
 	char * name;
 	int inoden;
-} dir_data_pair;
+} ddata_entry;
 
 typedef struct {
-	dir_data_pair[MAX_INODES] mdata;
-	int inum;
-	int offset;
-	void * next; //next block on the log
-} dir_data;
+	ddata_entry[MAX_INODES] map;
+} ddata;
 
 typedef enum {
 	FS_FILE, FS_DIR
@@ -43,12 +38,11 @@ typedef inode * pinode;
 
 typedef struct {
 	int inoden;
-	pinode * inode;
-} imap_pair;
+	pinode inode;
+} imap_entry;
 
 typedef struct {
-	imap_pair[MAX_INODES] map;
-	void * next;
+	imap_entry[MAX_INODES] map;
 } imap;
 
 typedef imap * pimap;
@@ -56,14 +50,29 @@ typedef imap * pimap;
 typedef struct {
 	char * dir_name;
 	int inoden;
-	pimap myimap;
-}cr_entry;
+	pimap map;
+} cr_entry;
 
 typedef struct {
 	cr_entry[MAX_IMAP] map;
 	disk_addr lstart;
 	disk_addr lend;
 } checkpoint;
+
+typedef struct {
+	FS_IMAP, FS_INODE
+} ntype;
+
+typedef struct {
+	ntype type;
+	ddata_addr next;
+	char data[0];
+} lnode;
+
+typedef struct {
+	unsigned short sector;
+	int offset;
+} disk_addr;
 
 // Hacemos el CR en RAM
 // reservar buffer en RAM
